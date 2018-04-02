890d6a5aa81916dcf0cfa3611410af61d7c5f217
Stewart <jds013@live.missouristate.edu>
2018-04-02 18:53:49 -0500

    Added input_data and euclideand_dist functions input_data: inputs data from text file and returns data as a list of lists. euclidean_dist: returns the euclidean distance of two list objects.

--- hw3.py
+++ hw3.py
@@ -7,17 +7,40 @@ Trace Folder: stewart013
 
 #---------------------------------Imports--------------------------------------
 import math
+#import csv
 #------------------------------------------------------------------------------
 
 
 
 #---------------------------------Variables------------------------------------
-
+K = 3
+clear = 100*'\n'
 #------------------------------------------------------------------------------
 
 
 
 #---------------------------------Classes/Functions----------------------------
+def input_data():
+    #Creates a list of lists of the input data
+    data = []
+    with open('synthetic_2D.txt', 'r') as inputfile:
+        for line in inputfile:
+            line = line.strip('\n')
+            line = str(line).split(' ')
+            data.append(line)
+    return data
+
+def euclidean_dist(curr_node, comp_node):
+    #print("train_item_data[0]: ", train_item_data[0])
+    squared_euclidean = []
+    euclidean_dist = 0
+    for num in range(0,len(curr_node)-1):
+        curr = float(curr_node[num])
+        comp = float(comp_node[num])
+        squared_euclidean.append((curr-comp)*(curr-comp))
+    for num in squared_euclidean:
+        euclidean_dist = euclidean_dist + num
+    return math.sqrt(euclidean_dist)
 
 #------------------------------------------------------------------------------
 
@@ -25,7 +48,9 @@ import math
 
 #---------------------------------Program Main---------------------------------
 def main():
-	
+    data = input_data()
+    print(data)
+    print("euclidean_dist: ", euclidean_dist(data[0],data[1]))
 	
 main()		
 #---------------------------------End of Program-------------------------------
