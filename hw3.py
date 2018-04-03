a574ea98fd270e847ba52843d30cc3a418c8a351
Spatz <brs519@live.missouristate.edu>
2018-04-03 11:14:51 -0500

    Changed variable names to be more 'pythonic'

--- hw3.py
+++ hw3.py
@@ -13,7 +13,7 @@ import math
 
 #---------------------------------Variables------------------------------------
 k = int # Integer holding number of clusters
-dataFile = "" # String holding name of file containing data
+data_file = "" # String holding name of file containing data
 data = [] # List holding lists for each line in dataFile
 #------------------------------------------------------------------------------
 
@@ -24,8 +24,8 @@ data = [] # List holding lists for each line in dataFile
     Takes name of data file as a parameter. Creates a list for each line
     in data file. Appends the lists to the data variable.
 """
-def getData(dataFile):
-    with open (dataFile) as f: #Closes file after data is gathered
+def get_data(data_file):
+    with open (data_file) as f: #Closes file after data is gathered
         lines = f.read().splitlines() #Splits at each new line
         #Creates a list for each line and converts data to floats
         for line in lines: 
@@ -36,13 +36,13 @@ def getData(dataFile):
     Takes the data as a parameter. Returns the number of unique cluster
     values found within the data. Variable k will be set to this value.
 """
-def numClusters(data):
+def num_clusters(data):
     count = int
-    tempList = [] #Holds list of all cluster values found in data
+    temp_list = [] #Holds list of all cluster values found in data
     for line in data:
-        tempList.append(line[2])
+        temp_list.append(line[2])
     #Use set to get unique values. Returns the length of the set.
-    count = len(set(tempList)) 
+    count = len(set(temp_list)) 
     return count
     
         
@@ -54,10 +54,10 @@ def numClusters(data):
 
 #---------------------------------Program Main---------------------------------
 def main():
-    dataFile = "synthetic_2D.txt"
-    getData(dataFile)
+    data_file = "synthetic_2D.txt"
+    get_data(data_file)
     print(data)
-    k = numClusters(data)
+    k = num_clusters(data)
     print(k)
 	
 main()		
