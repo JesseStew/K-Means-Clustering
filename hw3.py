d54c9325b6cdd86d155c9fabf314118e5e647c8e
Jesse Stewart <stewed.jesse@gmail.com>
2018-04-03 20:52:07 -0500

    Added assignItems function started calcNewMeans function assignItems assigns each node to the cluster which has the closest mean. Need to add update mean functionality in calcNewMeans and make it work with this function.

--- hw3.py
+++ hw3.py
@@ -1,55 +1,62 @@
 """ 
-Program: hw3.py
+Program:       hw3.py
 Programmed By: Brett Spatz and Jesse Stewart
-Description: K-Means Clustering Algorithm
-Trace Folder: stewart013
+Description:   K-Means Clustering Algorithm
+Trace Folder:  stewart013
 """
 
 #---------------------------------Imports--------------------------------------
 import math
+import random
 #------------------------------------------------------------------------------
 
 
 
 #---------------------------------Variables------------------------------------
-k = int # Integer holding number of clusters
-dataFile = "" # String holding name of file containing data
-data = [] # List holding lists for each line in dataFile
-clear = 100*'\n'
+k = int        # Integer holding number of clusters
+dataFile = ""  # String holding name of file containing data
+data = []      # List holding lists for each line in dataFile
+ranSeed = 90   # Arbitrary integer constant to seed random number generator
 #------------------------------------------------------------------------------
 
 
 
 #---------------------------------Classes/Functions----------------------------
 """
-    Takes name of data file as a parameter. Creates a list for each line
-    in data file. Appends the lists to the data variable.
+    Description: Creates a list for each line in data file. Appends the lists 
+                 to the data list.
+    Input:       Name of data file.
 """
 def getData(dataFile):
-    with open (dataFile) as f: #Closes file after data is gathered
-        lines = f.read().splitlines() #Splits at each new line
-        #Creates a list for each line and converts data to floats
+    with open (dataFile) as f:
+        lines = f.read().splitlines()
+        
+        # Creates a list for each line and converts data to floats
         for line in lines: 
-            line = [float(i) for i in line.split(' ')] #Float for calculations
-            data.append(line) #Append to data
+            line = [float(i) for i in line.split(' ')]
+            data.append(line)
+
 
 """
-    Takes the data as a parameter. Returns the number of unique cluster
-    values found within the data. Variable k will be set to this value.
+    Description: Finds unique clusters to determine value of k.
+    Input:       The pre-processed data (in the form of a list of lists).
+    Returns:     Number of unique clusters found within the data.
 """
 def numClusters(data):
     count = int
-    tempList = [] #Holds list of all cluster values found in data
+    tempList = [] # Holds list of all cluster values found in data
     for line in data:
         tempList.append(line[2])
-    #Use set to get unique values. Returns the length of the set.
+        
+    # Use set to get only unique values. Returns the length of the set.
     count = len(set(tempList)) 
     return count
 
+ 
 """
- Find euclidean distance measure of two nodes.
- Input: two nodes from data.
- Returns: euclidean distance of two nodes.
+    Description: Find Euclidean distance measure of two nodes.
+    Input:       Two nodes from data.
+    Returns:     Euclidean distance of two nodes.
 """
 def euclideanDist(curr_node, comp_node):
     squared_euclidean = []
@@ -62,6 +69,48 @@ def euclideanDist(curr_node, comp_node):
         euclidean_dist = euclidean_dist + itr
     return math.sqrt(euclidean_dist)
 
+
+"""
+    Description: Randomly determine the initial means.
+    Input:       Pre-processed data, number of means to find (k).
+    Returns:     List of initial k random nodes.
+"""   
+def setInitialMeans(k, data):
+    random.shuffle(data)
+    init_means = []
+    for num in range(k):
+        init_means.append(data[num])
+    return init_means
+
+"""
+    Description: Assigns each node to the cluster which has the closest mean.
+    Input:       Pre-processed data, initially selected means.
+    Returns:     Dictionary with cluster numbers as keys containing 
+                 nodes that belong to that cluster.
+"""   
+def assignItems(init_means, data):
+    clusters = {}
+    for itr in init_means:
+        clusters[itr[2]] = []   #initializes clusters as keys to lists
+    for itr in data:
+        curr_dist = math.inf    #large number to be updated
+        clust_num = float       #for storing the cluster of min dist from node
+        for i in init_means:
+            dist = euclideanDist(itr, i)
+            if(dist < curr_dist):
+                clust_num = i[2]
+                curr_dist = dist
+                node = itr
+        clusters[clust_num].append(node)
+    return clusters
+
+"""
+    Description: Calculates new mean for each cluster.
+    Input:       Dictionary with cluster numbers as keys containing 
+                 nodes that belong to that cluster.
+""" 
+def calcNewMeans(clusters):
+    
 #------------------------------------------------------------------------------
 
 
@@ -70,11 +119,15 @@ def euclideanDist(curr_node, comp_node):
 def main():
     dataFile = "synthetic_2D.txt"
     getData(dataFile)
-#    k = numClusters(data)
-    print(data)
-    print("euclidean_dist: ", euclideanDist(data[0],data[1]))
+    #print(data)
+    k = numClusters(data)
+    init_means = setInitialMeans(k, data)
+    clusters = assignItems(init_means, data)
     print("k: ", k)
-    print("numClusters: ", numClusters(data))
+    print("init_means: ", init_means)
+    print("clusters[init_means[0][2]]: ", clusters[init_means[0][2]])
+    print("\nclusters[init_means[1][2]]: ", clusters[init_means[1][2]])
+    print("\nclusters[init_means[2][2]]: ", clusters[init_means[2][2]])
 	
-main()		
+main()
 #---------------------------------End of Program-------------------------------
